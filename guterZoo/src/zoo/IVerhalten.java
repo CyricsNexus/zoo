0d0548a56b70dc91b073a5f7c313fccc7b17dc13
CyricsNexus <68751459+CyricsNexus@users.noreply.github.com>
2023-01-31 23:30:33 +0100

    Version 1.1

--- guterZoo/src/zoo/IVerhalten.java
+++ guterZoo/src/zoo/IVerhalten.java
@@ -1,5 +1,8 @@
 package zoo;
 
+// Schnittstelle wird in Klassen Affe, Loewe und Wolf implementiert
+// Dadurch MÜSSEN die Klassen die Methoden lautGeben(), laufen() implementieren
+
 public interface IVerhalten {
 	public String lautGeben();
 
