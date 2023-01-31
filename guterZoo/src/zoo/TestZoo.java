6e16db367134e09952431bff6cfba9792f1128fb
CyricsNexus <68751459+CyricsNexus@users.noreply.github.com>
2023-01-31 23:28:17 +0100

    Version 1.1
    Kommentare eingefügt

--- guterZoo/src/zoo/TestZoo.java
+++ guterZoo/src/zoo/TestZoo.java
@@ -3,17 +3,21 @@ package zoo;
 public class TestZoo {
 
 	public static void main(String[] args) {
-		// TODO Automatisch generierter Methodenstub
+		// Tier Hugo wird erzeugt und nutzt die Schnittstelle iVerhalten (Use-Beziehung) vom Wolf
+		// Die Schnittstelle realisiert die gesamten Methoden der Klassen, die die Schnittstelle implementiert haben
+		// (realize-Beziehung) und ordnet die Variablen mittels der Methoden der zugeh√∂rigen Klasse Wolf zu.
 		Zoo tier1 = new Zoo("Hugo",new Wolf());
-		System.out.printf("%s macht '%s' und l‰uft '%s'.%n", 
+		System.out.printf("%s macht '%s' und l√§uft '%s'.%n", 
 				tier1.getTiername(),
 				tier1.getLaut(),
 				tier1.getLaufen());
 				
+		// Sollte die Zuweisung √ºber Schnittstelle iVerhalten neu gesetzt werden, werden die Methoden nun aus der anderen
+		// Klasse genutzt. In diesem Fall werden bei tier1 nun die Methoden aus der Klasse Loewe genutzt
 		tier1.setTier(new Loewe());
-		System.out.printf("%s wurde ver‰ndert.%n", tier1.getTiername());
+		System.out.printf("%s wurde ver√§ndert.%n", tier1.getTiername());
 		
-		System.out.printf("%s macht '%s' und l‰uft '%s'.%n", 
+		System.out.printf("%s macht '%s' und l√§uft '%s'.%n", 
 				tier1.getTiername(),
 				tier1.getLaut(),
 				tier1.getLaufen());
