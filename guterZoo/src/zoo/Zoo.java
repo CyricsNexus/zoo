8e58fc4f33fe316c3eda4291002b5d41365e994b
CyricsNexus <68751459+CyricsNexus@users.noreply.github.com>
2023-02-01 08:28:23 +0100

    Version 1.1.2
    Kommentare hinzugefügt

--- guterZoo/src/zoo/Zoo.java
+++ guterZoo/src/zoo/Zoo.java
@@ -3,39 +3,38 @@ package zoo;
 
 public class Zoo {
 
+	// Variablendeklaration
 	private String tiername;
 	private IVerhalten tier = null;
 
 	
-	
+	// Konstructor
 	public Zoo(String tiername,IVerhalten iTier) {
 		this.tiername = tiername;
 		this.tier = iTier;
 
 	}
 
-	/*
-	 * Setter für Korrekturen
-	 */	
+	//Getter / Setter für Instanzvariable Tiername
+	public String getTiername(){
+		return tiername;
+	}
 	public void setTiername(String tiername) {
 		this.tiername = tiername;
 	}
 	
+	// nachträge Zuweisung über die Schnittstelle
 	public void setTier(IVerhalten tier) {
 		this.tier = tier;
 	}
 
-	
+	// zur Verfügung gestellte Methoden aus der Schnittstelle (Use-Beziehung
 	public String lautGeben(){
 		return tier.lautGeben();
 	}
 	
 	public String laufen(){
 		return tier.laufen();
-	}
-	
-	public String getTiername(){
-		return tiername;
 	}
 
 }
